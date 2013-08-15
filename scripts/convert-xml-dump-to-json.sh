91c95e8d4b9938f486eec41208735295a864715f
diegoceccarelli <diego.ceccarelli@gmail.com>
2013-08-15 13:11:38 +0200

    fixed scripts names, added language

---
+++ scripts/convert-xml-dump-to-json.sh
@@ -0,0 +1,22 @@
+
+#!/usr/bin/env bash
+
+EXPECTED_ARGS=3
+E_BADARGS=65
+
+source scripts/config.sh
+
+if [ $# -ne $EXPECTED_ARGS ];
+then
+  echo "Usage: `basename $0` lang[en,it] xml-dump  json-dump "
+  exit $E_BADARGS
+fi
+
+LANG=$1
+WIKI_XML_DUMP=$2
+WIKI_JSON_DUMP=$3
+
+
+echo "converting mediawiki xml dump to json dump ($WIKI_JSON_DUMP)"
+
+$JAVA  it.isti.cnr.hpc.wikipedia.cli.MediawikiToJsonCLI -input $WIKI_XML_DUMP -output $WIKI_JSON_DUMP -lang $LANG
